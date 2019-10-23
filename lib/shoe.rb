96b88411289e56b5c0de5caa973f09f1b11d096d
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-23 00:07:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,38 @@
-# Make your shoe class here!
+class Book
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def brand
+    @brand
+  end
+ 
+  def color=(color)
+    @color = color
+  end
+ 
+  def color
+    @color
+  end
+ 
+  def size=(num)
+    @size = num
+  end
+ 
+  def size
+    @size
+  end
+ 
+  def material=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
